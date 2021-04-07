2e35acf4a76e24f8709d4c6870f6fff43a291537
裴亚辉 <291410026@qq.com>
2021-04-07 16:18:46 +0800

    refator: cicd36

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,7 +26,7 @@ router.get('/api/db-check', async (ctx, next) => {
     ctx.body = {
         errno: 0,
         data: {
-            name: 'biz-editor-!!!123123123qwe',
+            name: 'biz-editor-!!!3',
             version: packageInfo.version,
             ENV,
             redisConn: redisTestVal != null,
